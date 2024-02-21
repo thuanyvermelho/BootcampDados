8450dce9df08654894862161512da75aa0c89cb2
Thuany Vermelho <thuanyvermelho@gmail.com>
2024-02-21 19:14:26 -0300

    Primeiro Commit

---
+++ Aula01/exemplo01.py
@@ -0,0 +1,11 @@
+# Solicita ao usuário que digite seu nome
+nome = input("Digite seu nome: ")
+
+# Calcula o número de caracteres do nome
+num_caracteres = len(nome)
+
+# Exibe o número de caracteres
+print(f"O número de caracteres do seu nome é: {num_caracteres}")
+
+
+
---
+++ Aula01/exemplo02.py
@@ -0,0 +1,7 @@
+valor01 = float(input("Digite um numero: "))
+
+valor02 = float(input("Digite um numero: "))
+
+soma = valor01 + valor02
+
+print(f"A soma do {valor01} com o {valor02} é igual a {soma}")
