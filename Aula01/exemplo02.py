cc0b1054d3288de1bbcd3579555552afc31d6608
Thuany Vermelho <thuanyvermelho@gmail.com>
2024-02-21 19:07:00 -0300

    Remove todos os arquivos do repositório

--- Aula01/exemplo02.py
+++
@@ -1,7 +0,0 @@
-valor01 = float(input("Digite um numero: "))
-
-valor02 = float(input("Digite um numero: "))
-
-soma = valor01 + valor02
-
-print(f"A soma do {valor01} com o {valor02} é igual a {soma}")
